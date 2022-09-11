358a047b9889f235d2321f39d2bf01c8c02ac129
gitlaws <lawmski@gmail.com>
2022-09-11 17:42:14 -0400

    added footer component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+describe('App Component', () => {
+    it('Exists', () => {
+        //assert
+        expect(AppComponent).toBeDefined();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('Can be built', () => {
+        //act
+        const component = new AppComponent();
 
-  it(`should have as title 'git-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('git-app');
-  });
+        //assert
+        expect(component instanceof AppComponent).toBe(true);
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('git-app app is running!');
-  });
 });
